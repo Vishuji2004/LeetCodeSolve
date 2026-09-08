e54bdf0704ad96530ba8377f52820acff9574ba7
Vishuji2004 <shuklavishu056@gmail.com>
2026-09-09 01:48:32 +0530

    Time: 465 ms (6.98%), Space: 45.2 MB (17.86%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    unordered_map<int, int> dp;
+    int numSquares(int n) {
+        if(n == 0) return 0;
+        if(dp.contains(n)) return dp[n];
+        int ans = n;
+        for(int i = 1; i*i <= n; i++){
+            ans = min(ans, 1 + numSquares(n-i*i));
+        }
+        return dp[n] = ans;
+    }
+};
