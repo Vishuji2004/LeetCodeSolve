cb373e381395baee724d53605f33f5111f7bdad1
Vishuji2004 <shuklavishu056@gmail.com>
2026-09-14 16:08:34 +0530

    Time: 1 ms (19.99%), Space: 9.8 MB (8.4%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> f1(26, 0), f2(26, 0);
-        for(char c : s){
-            f1[c - 'a']++;
+        string n = s + t;
+        int c = 0;
+        for(char ch : n){
+            c ^= (ch - 'a');
         }
-
-        for(char c : t) {
-            f2[c - 'a']++;
-        }
-
-        for(int i = 0; i < 26; i++){
-            if(f1[i] != f2[i]) return 'a' + i;
-        }
-
-        return ' ';
+        return c + 'a';
     }
 };
